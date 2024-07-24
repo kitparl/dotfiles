26879025ca87eb036d4ac510f88bf046151a1b4a
pranshu.bisht <pranshu.bisht@tc-pl.in>
2024-07-24 22:41:23 +0530

    added more

---
+++ .fzf.bash
@@ -0,0 +1,7 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/kitparl/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/kitparl/.fzf/bin"
+fi
+
+eval "$(fzf --bash)"
---
+++ .fzf.zsh
@@ -0,0 +1,5 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/kitparl/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/kitparl/.fzf/bin"
+fi
