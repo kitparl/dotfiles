d3a1ef94c5e36c7a3d5e05d6405120e383fbea7d
pranshu.bisht <pranshu.bisht@tc-pl.in>
2024-08-03 23:23:05 +0530

    improve alias & nvim plugin added

--- .config/nvim/lua/plugins/example.lua
+++ .config/nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-LazyVimif true then return {} end
+if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -13,7 +13,7 @@ return {
   { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
-  {LazyVim
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox",
